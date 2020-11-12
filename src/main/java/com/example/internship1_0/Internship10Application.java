b53755c6f18ed313eaeb99813ad622d89a0b2dd9
HelloWorld <michalon@post.bgu.ac.il>
2020-11-12 11:13:21 +0200

    Initial commit

---
+++ src/main/java/com/example/internship1_0/Internship10Application.java
@@ -0,0 +1,13 @@
+package com.example.internship1_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Internship10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Internship10Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/internship1_0/Internship10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internship1_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Internship10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
