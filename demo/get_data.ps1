41a49b978d5ed57d7b6273fb84550a1a43c886c2
YaelJannai <57910052+YaelJannai@users.noreply.github.com>
2020-11-18 18:04:00 +0200

    Add files via upload
    test with spring web page, presenting output of powersell script

---
+++ demo/get_data.ps1
@@ -0,0 +1,15 @@
+﻿Install-Module MSOnline -Force
+
+$UserCredential = Get-Credential
+
+Connect-MsolService -Credential $UserCredential
+
+# Get-MsolUser -UserPrincipalName "yjannai@dalet.com" | Select-Object *| Format-List
+# Get-MsolUser -UserPrincipalName "yjannai@dalet.com" | Select-Object *| Format-List | Export-CSV c:\o365userlist.csv –NoTypeInformation
+# Get-MsolUser -UserPrincipalName "amichaeli@dalet.com"
+$david = Get-MsolUser -SearchString "David"
+foreach ($d in $david)
+{
+    $d
+}
+# Get-MsolAccountSku | Where-Object {$_.SkuPartNumber -eq “ENTERPRISEPACK”} | ForEach-Object {$_.ServiceStatus}
---
+++ demo/src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import com.profesorfalken.jpowershell.PowerShell;
+import com.profesorfalken.jpowershell.PowerShellNotAvailableException;
+import com.profesorfalken.jpowershell.PowerShellResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("/data")
+    public String data(Model model) {
+        GreetingController runScript = new GreetingController();
+        String response = runScript.script();
+        model.addAttribute("response", response);
+        return "data";
+    }
+
+    public String script() {
+        try (PowerShell powerShell = PowerShell.openSession()) {
+            //Increase timeout to give enough time to the script to finish
+            Map<String, String> config = new HashMap<>();
+            config.put("maxWait", "80000");
+
+            //Execute script
+            PowerShellResponse response = powerShell.configuration(config).executeScript("get_data.ps1");
+
+            //Print results if the script
+            return ("Script output:" + response.getCommandOutput());
+        } catch(PowerShellNotAvailableException ex) {
+            //Handle error when PowerShell is not available in the system
+            //Maybe try in another way?
+        }
+
+
+        return null;
+    }
+
+}
