3357f90dde5803524f6c0ae6142b5832d8578fa1
HelloWorld <michalon@post.bgu.ac.il>
2020-12-07 15:12:49 +0200

    Alon: updating packaging

---
+++ csv-api1/get_data.ps1
@@ -0,0 +1,17 @@
+﻿Install-Module MSOnline -Force
+
+$UserCredential = Get-Credential
+
+Connect-MsolService -Credential $UserCredential
+
+Get-MsolUser -SearchString "a" | Select SignInName, DisplayName, IsLicensed, UserType, WhenCreated |  Export-CSV o365userlist.csv -NoTypeInformation
+
+# Get-MsolUser -UserPrincipalName "yjannai@dalet.com" | Select-Object *| Format-List
+# Get-MsolUser -UserPrincipalName "yjannai@dalet.com" | Select-Object *| Format-List | Export-CSV c:\o365userlist.csv –NoTypeInformation
+# Get-MsolUser -UserPrincipalName "amichaeli@dalet.com"
+# $david = Get-MsolUser -SearchString "David"
+# foreach ($d in $david)
+# {
+#     $d
+# }
+# Get-MsolAccountSku | Where-Object {$_.SkuPartNumber -eq “ENTERPRISEPACK”} | ForEach-Object {$_.ServiceStatus}
